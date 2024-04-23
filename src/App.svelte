6c3c265976406d1edc13b1d480647bd3667e5a08
victormg <victor.mejia@clubhub.ai>
2024-04-23 15:14:24 -0500

    firts commit

---
+++ src/App.svelte
@@ -0,0 +1,45 @@
+<script>
+	import About from "./components/About.svelte";
+	export let name;
+	export let lastName;
+	let svelterLogo =  "https://upload.wikimedia.org/wikipedia/commons/1/1b/Svelte_Logo.svg"
+</script>
+
+<main>
+	<h1>Hello {name}! {lastName}</h1>
+	<About/>
+	<img src="{svelterLogo}" alt="svelte">
+</main>
+
+<style>
+	:global(body) {
+		background-color: #f2eee2;
+		color : #0048f6;
+	}
+
+	:global(:root) {
+		--them-color: purple;
+	}
+	main img {
+		width: 30%;
+	}
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/components/About.svelte
@@ -0,0 +1,19 @@
+<script>
+    let someText = `Backend developer`
+    let count = 0
+
+    function handleClick() {
+        count += 1
+    }
+</script>
+
+<style>
+    p {
+        color: var(--them-color);
+    }
+</style>
+
+<div class="about">
+    <p>{someText}</p>
+    <button on:click={handleClick}>Click {count}</button>
+</div>
